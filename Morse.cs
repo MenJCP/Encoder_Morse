f5cdc35944ed5f9145dbb8ad727e6277b42a43c3
Jean C. Mendoza <73866099+MenJCP@users.noreply.github.com>
2021-06-15 16:34:25 -0400

    primer commit

---
+++ Morse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encode_Morse
+{
+   public class Morse
+    {
+        public string Translate(string word)
+        {
+            char[] chars = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            string[] morse = { " ", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "--.--", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--..", "---..", "----." };
+
+            string wordmorse = "";
+            word = word.ToLower();
+
+            for(int i=0; i < word.Length; i++)
+            {
+                for(int j=0; j < 38; j++)
+                {
+                    if(word[i]== chars[j])
+                    {
+                        wordmorse += morse[j];
+                        wordmorse += " ";
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("La palabra en codigo morse es: ");
+            return wordmorse;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Encode_Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Morse morse = new Morse();
+            Console.WriteLine("Ingrese la palabra a ser llevada a codigo morse: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(morse.Translate(word));
+            Console.ReadKey();
+        }
+    }
+}
